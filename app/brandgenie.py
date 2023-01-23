46bdb59080433afd902436df4419f391ff475653
Hamza <hamza.lbelghiti@gmail.com>
2023-01-23 23:32:22 +0100

    Added generate_image using dalle & modified generate_brande_name

--- app/brandgenie.py
+++ app/brandgenie.py
@@ -3,6 +3,10 @@ import openai
 import argparse
 import re
 
+# Load OPENAI_API_KEY
+from dotenv import load_dotenv
+load_dotenv()
+
 MAX_INPUT_LENGTH = 12
 
 def main():
@@ -14,8 +18,9 @@ def main():
     if validate_user_input(user_input):
 
         generate_brand_name(user_input)
-        generate_brand_snippet(user_input)
+        generate_brand_slogan(user_input)
         generate_keywords(user_input)
+        generate_image(user_input)
         
     else:
         raise ValueError(f"Input length is too long. Must be under {MAX_INPUT_LENGTH}.")
@@ -28,29 +33,40 @@ def generate_brand_name(user_input: str) -> str:
     # Load your API key from an environment variable or secret management service
     openai.api_key = os.getenv("OPENAI_API_KEY")
 
-    prompt = f"Generate unique and catchy brand name for an ecommerce website that sells {user_input}: "
+    prompt = f"Generate unique, original and creative brand name for an ecommerce website that sells {user_input}: "
     print(prompt)
 
     response = openai.Completion.create(model="text-davinci-001", prompt=prompt, temperature=0, max_tokens=32)
     brand_name = response["choices"][0]["text"].strip()
 
-    print(f"Brand Name : {brand_name}")
-    return brand_name
+    # In case there's multiple choices, take the first one
+    if '.' in brand_name:
+        # Split the string by newline characters and access the first element
+        first_brand_name = brand_name.split('\n')[0]
+        # Extracting the first word from the string 
+        result = first_brand_name.split('.')[1]
+        # Removing leading whitespace
+        result = result.strip()
+    else:
+        result = brand_name
+
+    print(f"Brand Name : {result}")
+    return result
     
 
-def generate_brand_snippet(user_input: str) -> str:
+def generate_brand_slogan(user_input: str) -> str:
 
     # Load your API key from an environment variable or secret management service
     openai.api_key = os.getenv("OPENAI_API_KEY")
 
-    prompt = f"Generate unique and short branding snippet for an ecommerce website that sells {user_input}: "
+    prompt = f"Generate unique, original, creative and short brand slogan for an ecommerce website that sells {user_input}: "
     print(prompt)
 
-    response = openai.Completion.create(model="text-davinci-001", prompt=prompt, temperature=0, max_tokens=32)
-    brand_snippet = response["choices"][0]["text"].strip()
+    response = openai.Completion.create(model="text-davinci-002", prompt=prompt, temperature=0, max_tokens=32)
+    brand_slogan = response["choices"][0]["text"].strip()
 
-    print(f"Branding Snippet : {brand_snippet}")
-    return brand_snippet
+    print(f"Branding Slogan : {brand_slogan}")
+    return brand_slogan
 
 
 def generate_keywords(user_input: str):
@@ -74,6 +90,31 @@ def generate_keywords(user_input: str):
 
     print(f"Branding Snippet : {keywords_array}")
     return keywords_array 
+
+
+def generate_image(user_input: str):
+
+    # Load your API key from an environment variable or secret management service
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+
+    prompt = f"Generate a high quality art work of {user_input}, digital art:"
+    print(prompt)
+
+    try:
+        response = openai.Image.create(
+            prompt=prompt,
+            n=1,
+            size="512x512"
+        )
+
+        image_url = response['data'][0]['url']
+        print(image_url)
+
+    except openai.error.OpenAIError as e:
+        print(e.http_status)
+        print(e.error)
+
+    return image_url
 
 if __name__ == "__main__":
     main()
--- app/brandgenie_api.py
+++ app/brandgenie_api.py
@@ -1,32 +1,42 @@
-from brandgenie import generate_brand_name, generate_brand_snippet, generate_keywords
+from brandgenie import generate_brand_name, generate_brand_slogan, generate_keywords, generate_image
 from fastapi import FastAPI, Query
+from mangum import Mangum
 
 MAX_INPUT_LENGTH = 12
 
 app = FastAPI()
+handler = Mangum(app)
 
 @app.get("/generate_brandname")
 async def generate_brandname(input: str = Query(max_length=MAX_INPUT_LENGTH)):
 
     brand_name = generate_brand_name(input)
-    return {"brand_name": brand_name, "brand_snippet": None, "keywords": []}
+    return {"brand_name": brand_name, "brand_slogan": None, "keywords": [], "brand_image": None}
 
-@app.get("/generate_brandsnippet")
-async def generate_brandsnippet(input: str = Query(max_length=MAX_INPUT_LENGTH)):
+@app.get("/generate_brandslogan")
+async def generate_brandslogan(input: str = Query(max_length=MAX_INPUT_LENGTH)):
 
-    brand_snippet = generate_brand_snippet(input)
-    return {"brand_name": None, "brand_snippet": brand_snippet, "keywords": []}
+    brand_slogan = generate_brand_slogan(input)
+    return {"brand_name": None, "brand_slogan": brand_slogan, "keywords": [], "brand_image": None}
 
 @app.get("/generate_brandkeywords")
 async def generate_brandkeywords(input: str = Query(max_length=MAX_INPUT_LENGTH)):
 
     keywords = generate_keywords(input)
-    return {"brand_name": None, "brand_snippet": None, "keywords": keywords}
+    return {"brand_name": None, "brand_slogan": None, "keywords": keywords, "brand_image": None}
+
+@app.get("/generate_brandimage")
+async def generate_brandimage(input: str = Query(max_length=MAX_INPUT_LENGTH)):
+
+    image_url = generate_image(input)
+    return {"brand_name": None, "brand_slogan": None, "keywords": None, "brand_image": image_url}
 
 @app.get("/generate_branding")
 async def generate_branding(input: str = Query(max_length=MAX_INPUT_LENGTH)):
 
     brand_name = generate_brand_name(input)
-    brand_snippet = generate_brand_snippet(input)
+    brand_slogan = generate_brand_slogan(input)
     keywords = generate_keywords(input)
-    return {"brand_name": brand_name, "brand_snippet": brand_snippet, "keywords": keywords}
+    image_url = generate_image(input)
+    return {"brand_name": brand_name, "brand_slogan": brand_slogan, "keywords": keywords, "brand_image": image_url}
+
