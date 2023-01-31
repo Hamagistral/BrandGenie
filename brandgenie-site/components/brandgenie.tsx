8a866aa1a640f5415f7613bd418cf288634c7e87
Hamza <hamza.lbelghiti@gmail.com>
2023-01-31 22:42:26 +0100

    Modified padding

--- brandgenie-site/components/brandgenie.tsx
+++ brandgenie-site/components/brandgenie.tsx
@@ -72,8 +72,8 @@ const BrandGenie: React.FC = () => {
 
   return (
     <>
-      <div className="flex h-screen py-4">
-          <div className="max-w-lg m-auto p-8 relative isolate overflow-hidden bg-slate-900 shadow-lg rounded-xl">
+      <div className="flex h-screen p-4">
+          <div className="max-w-lg m-auto p-8  relative isolate overflow-hidden bg-slate-900 shadow-lg rounded-xl">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" className="absolute top-1/2 left-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:translate-y-0 lg:-translate-x-1/2" aria-hidden="true">
               <circle cx="512" cy="512" r="512" fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fill-opacity="0.7" />
               <defs>
