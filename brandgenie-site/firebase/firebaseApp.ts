efc2d34b402bf3a741894b6720982bd55b65a37c
Hamza <hamza.lbelghiti@gmail.com>
2023-02-06 18:39:18 +0100

    Added Firebase config

--- brandgenie-site/firebase/firebaseApp.ts
+++ brandgenie-site/firebase/firebaseApp.ts
@@ -1,15 +1,6 @@
 import { initializeApp } from "firebase/app";
 import 'firebase/firestore';
-
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
-};
+import { firebaseConfig } from '../firebase.config';
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
