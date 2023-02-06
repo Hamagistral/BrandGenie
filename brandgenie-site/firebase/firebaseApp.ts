b578e1be15c3578cbf5061407bd3f2e6b778ad31
Hamza <hamza.lbelghiti@gmail.com>
2023-02-06 18:24:52 +0100

    Added Firebase Auth

--- brandgenie-site/firebase/firebaseApp.ts
+++ brandgenie-site/firebase/firebaseApp.ts
@@ -1,4 +1,3 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import 'firebase/firestore';
