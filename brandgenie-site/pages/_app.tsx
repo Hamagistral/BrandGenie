fa8060bbaefaa1b34f8a95b92ad0d8581e6fe331
Hamza <hamza.lbelghiti@gmail.com>
2023-05-25 00:47:13 +0100

    added meta data & google analytics

--- brandgenie-site/pages/_app.tsx
+++ brandgenie-site/pages/_app.tsx
@@ -1,6 +1,24 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
+
+const measurementId = process.env.GOOGLE_ANALYTICS;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<>
+
+    <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
+    />
+    <Script id="google-analytics" strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${measurementId}');
+        `}
+    </Script>
+    <Component {...pageProps} />
+  </>)
 }
--- brandgenie-site/pages/index.tsx
+++ brandgenie-site/pages/index.tsx
@@ -6,7 +6,12 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>BrandGenie</title>
+        <title>BrandGenie | AI Branding Assistant</title>
+        <meta
+          name="description"
+          content="Get a complete brand package for your e-commerce site with just a product description. Includes name, slogan, ad copy, and related keywords."
+          key="desc"
+        />
         <meta name="description" content="AI Generated Branding" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/brandgenieicon.ico" />
